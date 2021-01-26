c0a574559733bcdd921c16b021176ec1e254260f
Herring <reflax0@gmail.com>
2021-01-26 14:30:46 +0300

    initial commit

---
+++ src/main/java/com/herring/faubook/FaubookApplication.java
@@ -0,0 +1,13 @@
+package com.herring.faubook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaubookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaubookApplication.class, args);
+	}
+
+}

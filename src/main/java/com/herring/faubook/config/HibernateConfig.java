3fab6b1f007d61c41225fdf6b933bd751cdc3b28
Herring <reflax0@gmail.com>
2021-01-26 15:06:55 +0300

    commit

---
+++ src/main/java/com/herring/faubook/config/HibernateConfig.java
@@ -0,0 +1,17 @@
+package com.herring.faubook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+
+}
